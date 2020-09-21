bb7135405318e3da5e5a1dcc654f92129226cab8
MixZira <58696475+MixZira@users.noreply.github.com>
2020-09-21 17:37:17 -0300

    ZIRIX V2

--- zirix-data/resources/[emp]/[miner]/emp_miner-store/nui/lightsaber.js
+++ zirix-data/resources/[emp]/[miner]/emp_miner-store/nui/lightsaber.js
@@ -66,7 +66,7 @@ $(function() {
   }
   
   function sendData(name, data) {
-	$.post("http://nav_loja-minerio/" + name, JSON.stringify(data), function(
+	$.post("http://emp_miner-store/" + name, JSON.stringify(data), function(
 	  datab
 	) {
 	  if (datab != "ok") {
