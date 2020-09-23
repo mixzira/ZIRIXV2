91da50b408d8b956fc54443144685fd5befb71fe
MixZira <58696475+MixZira@users.noreply.github.com>
2020-09-23 17:25:53 -0300

    ZIRIX V2

--- zirix-data/resources/[vrp]/vrp_doors/hansolo/hansolo.lua
+++ zirix-data/resources/[vrp]/vrp_doors/hansolo/hansolo.lua
@@ -2,7 +2,10 @@ local Tunnel = module("vrp","lib/Tunnel")
 local Proxy = module("vrp","lib/Proxy")
 vRP = Proxy.getInterface("vRP")
 
+vRPNserver = Tunnel.getInterface("vrp_doors")
+
 local doors = {}
+local hour = 0
 
 RegisterNetEvent('vrpdoorsystem:load')
 AddEventHandler('vrpdoorsystem:load',function(list)
@@ -19,40 +22,77 @@ end)
 function searchIdDoor()
 	local x,y,z = table.unpack(GetEntityCoords(PlayerPedId()))
 	for k,v in pairs(doors) do
-		if GetDistanceBetweenCoords(x,y,z,v.x,v.y,v.z,true) <= 1.5 then
-			return k
+		if v.public then
+			return 0
+		else
+			if GetDistanceBetweenCoords(x,y,z,v.x,v.y,v.z,true) <= 1.5 then
+				return k
+			end
 		end
 	end
 	return 0
 end
 
-Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(1)
-		if IsControlJustPressed(0,38) then
-			local id = searchIdDoor()
-			if id ~= 0 then
-				vRP._playAnim(true,{{"veh@mower@base","start_engine"}},false)
-				Citizen.Wait(2200)
-				TriggerServerEvent("vrpdoorsystem:open",id)
+function searchPublicIdDoor()
+	local x,y,z = table.unpack(GetEntityCoords(PlayerPedId()))
+	for k,v in pairs(doors) do
+		if v.public then
+			if GetDistanceBetweenCoords(x,y,z,v.x,v.y,v.z,true) < 5.1 then
+				return k
 			end
 		end
 	end
-end)
+	return 0
+end
+
+function CalculateTimeToDisplay()
+	hour = GetClockHours()
+	if hour <= 9 then
+		hour = "0" .. hour
+	end
+end
 
 Citizen.CreateThread(function()
 	while true do
 		local idle = 1000
 		local x,y,z = table.unpack(GetEntityCoords(PlayerPedId()))
+		CalculateTimeToDisplay()
+
+		local publicId = searchPublicIdDoor()
+		if publicId ~= 0 then
+			if parseInt(hour) >= 07 and parseInt(hour) <= 17 then
+				vRPNserver.timeOpen(publicId)
+			else
+				vRPNserver.timeClose(publicId)
+			end
+
+			if IsControlJustPressed(0,38) and vRPNserver.checkItemTime(publicId) then
+				vRP._playAnim(true,{{"veh@mower@base","start_engine"}},false)
+				Citizen.Wait(2200)
+				vRPNserver.forceOpen(publicId)
+			end
+		end
+		
+		local id = searchIdDoor()
+		if id ~= 0 then
+			if IsControlJustPressed(0,38) then
+				vRP._playAnim(true,{{"veh@mower@base","start_engine"}},false)
+				Citizen.Wait(2200)
+				TriggerServerEvent("vrpdoorsystem:open",id)
+			end
+		end
+		
 		for k,v in pairs(doors) do
-			if GetDistanceBetweenCoords(x,y,z,v.x,v.y,v.z,true) < 3 then
+			if GetDistanceBetweenCoords(x,y,z,v.x,v.y,v.z,true) < 5 then
 				idle = 5
 				local door = GetClosestObjectOfType(v.x,v.y,v.z,1.0,v.hash,false,false,false)
 				if door ~= 0 then
 					SetEntityCanBeDamaged(door,false)
 					if v.lock == false then
 						if v.text then
-							DrawText3Ds(v.x,v.y,v.z+0.2,"[~g~E~w~]  FECHAR")
+							if not v.public then
+								DrawText3Ds(v.x,v.y,v.z+0.2,"[~p~E~w~] Porta ~p~destrancada~w~.")
+							end
 						end
 						NetworkRequestControlOfEntity(door)
 						FreezeEntityPosition(door,false)
@@ -60,7 +100,9 @@ Citizen.CreateThread(function()
 						local lock,heading = GetStateOfClosestDoorOfType(v.hash,v.x,v.y,v.z,lock,heading)
 						if heading > -0.02 and heading < 0.02 then
 							if v.text then
-								DrawText3Ds(v.x,v.y,v.z+0.2,"[~g~E~w~]  ABRIR")
+								if not v.public then
+									DrawText3Ds(v.x,v.y,v.z+0.2,"[~p~E~w~] Porta ~p~trancada~w~.")	
+								end
 							end
 							NetworkRequestControlOfEntity(door)
 							FreezeEntityPosition(door,true)
@@ -76,12 +118,13 @@ end)
 function DrawText3Ds(x,y,z,text)
 	local onScreen,_x,_y = World3dToScreen2d(x,y,z)
 	SetTextFont(4)
-	SetTextScale(0.35,0.35)
+	SetTextScale(0.25,0.25)
 	SetTextColour(255,255,255,150)
 	SetTextEntry("STRING")
 	SetTextCentre(1)
 	AddTextComponentString(text)
+
 	DrawText(_x,_y)
-	local factor = (string.len(text))/370
-	DrawRect(_x,_y+0.0125,0.01+factor,0.03,0,0,0,80)
+    local factor = (string.len(text)) / 370
+    DrawRect(_x,_y+0.0115, 0.001+factor, 0.03, 0, 0, 0,80)
 end
--- zirix-data/resources/[vrp]/vrp_doors/skywalker.lua
+++ zirix-data/resources/[vrp]/vrp_doors/skywalker.lua
@@ -11,6 +11,7 @@ Proxy.addInterface("vrp_doors",vRPN)
 --[ VARIABLES ]-----------------------------------------------------------------------------------------------------------------
 
 local cfg = module("vrp_doors","config/config")
+local timers = {}
 
 --[ SPAWN | EVENT ]-------------------------------------------------------------------------------------------------------------
 
@@ -37,3 +38,80 @@ AddEventHandler('vrpdoorsystem:open',function(id)
 		end
 	end
 end)
+
+--[ TIMERS ID | THREAD ]--------------------------------------------------------------------------------------------------------
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(1000)
+		for k,v in pairs(timers) do
+			if v > 0 then
+				timers[k] = v - 1
+			end
+		end
+	end
+end)
+
+--[ OPEN DOOR TIME | FUNCTION ]-------------------------------------------------------------------------------------------------
+
+function vRPN.timeOpen(id)
+	TriggerClientEvent('vrpdoorsystem:statusSend',-1,id,false)
+	if cfg.list[id].other ~= nil then
+		local idsecond = cfg.list[id].other
+		cfg.list[idsecond].lock = cfg.list[id].lock
+		TriggerClientEvent('vrpdoorsystem:statusSend',-1,idsecond,false)
+	end
+end
+
+--[ CLOSE DOOR TIME | FUNCTION ]------------------------------------------------------------------------------------------------
+
+function vRPN.timeClose(id)
+	if timers[id] == 0 or not timers[id] then
+		TriggerClientEvent('vrpdoorsystem:statusSend',-1,id,true)
+		if cfg.list[id].other ~= nil then
+			local idsecond = cfg.list[id].other
+			cfg.list[idsecond].lock = cfg.list[id].lock
+			TriggerClientEvent('vrpdoorsystem:statusSend',-1,idsecond,true)
+		end
+	else
+		TriggerClientEvent('vrpdoorsystem:statusSend',-1,id,false)
+		if cfg.list[id].other ~= nil then
+			local idsecond = cfg.list[id].other
+			cfg.list[idsecond].lock = cfg.list[id].lock
+			TriggerClientEvent('vrpdoorsystem:statusSend',-1,idsecond,false)
+		end
+	end
+end
+
+--[ FORCE OPEN DOOR | FUNCTION ]------------------------------------------------------------------------------------------------
+
+function vRPN.forceOpen(id)
+	if timers[id] == 0 or not timers[id] then
+		timers[id] = 120
+		TriggerClientEvent('vrpdoorsystem:statusSend',-1,id,false)
+		if cfg.list[id].other ~= nil then
+			local idsecond = cfg.list[id].other
+			cfg.list[idsecond].lock = cfg.list[id].lock
+			TriggerClientEvent('vrpdoorsystem:statusSend',-1,idsecond,false)
+		end
+	end
+end
+
+--[ CHECK TIME AND ITEM | FUNCTION ]--------------------------------------------------------------------------------------------
+
+function vRPN.checkItemTime(id)
+	local source = source
+	local user_id = vRP.getUserId(source)
+
+	if timers[id] == 0 or not timers[id] then
+		if vRP.tryGetInventoryItem(user_id,"lockpick",1) then
+			return true
+		else
+			TriggerClientEvent("Notify",source,"negado","Precisa de <b>Lockpick</b>.")
+			return false
+		end
+	else
+		TriggerClientEvent("Notify",source,"negado","Essa porta <b>já está destrancada</b> por <b>"..timers[id].." segundos</b>.")
+		return false
+	end
+end
