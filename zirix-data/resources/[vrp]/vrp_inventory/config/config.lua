1c7805e75eb8a1a6c74a79bfcce515ca1f1a656f
MixZira <58696475+MixZira@users.noreply.github.com>
2020-12-14 22:51:10 -0300

    Update config.lua

--- zirix-data/resources/[vrp]/vrp_inventory/config/config.lua
+++ zirix-data/resources/[vrp]/vrp_inventory/config/config.lua
@@ -9,4 +9,4 @@ config.webhookColor = 16431885
 config.webhookIcon = 'https://i.imgur.com/5ydYKZg.png'
 config.webhookBottom = 'ZIRIX V2 - '
 
-config.imageServer = 'http://192.99.251.232:3571'
+config.imageServer = ''
