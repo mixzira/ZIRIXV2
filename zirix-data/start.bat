0406db643cd2ec1f0df7630463a806dd36c41604
Mateus Magalhães <58696475+MixZira@users.noreply.github.com>
2021-02-01 09:02:46 -0300

    Update start.bat

--- zirix-data/start.bat
+++ zirix-data/start.bat
@@ -3,7 +3,7 @@
 echo ===-------------------------------===
 echo     ZIRIX V2 (0.2.0)
 echo     Developed by: ZIRAFLIX
-echo     Discord: discord.gg/ziraflix
+echo     Discord: discord.gg/MgvQnUaTJ4
 echo     Contact: contato@ziraflix.com
 echo ===-------------------------------===
 
